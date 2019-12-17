679c071f002edddb3d425ea19e08b27ef2e3d9bc
Danis Yogaswara <danistutorial@gmail.com>
2019-12-17 15:36:42 +0700

    add auth middleware

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/accounts', 'HomeController@index')->name('home');
-Route::get('/settings', 'Settings\SettingController@index')->name('setting.view');
+Route::get('/accounts', 'HomeController@index')->name('home.accounts');
+Route::get('/settings', 'HomeController@index')->name('setting.view');
